09afd1276a331cf978f15478993b4a1479719341
Darcy <github@darcythomas.com>
2018-11-22 22:55:36 +1300

    config and stuff

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Akka.Actor;
+using Akka.Persistence;
+using System;
+using System.Collections.Generic;
 
 namespace AkkaSpike
 {
@@ -7,6 +10,45 @@ namespace AkkaSpike
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+          
+
+            using (var system = ActorSystem.Create("my-actor-server"))
+            {
+                //start two services
+                var service1 = system.ActorOf<PlaceOrder>($"{nameof(PlaceOrder)}");
+              //  var service2 = system.ActorOf<Service2>("service2");
+                Console.ReadKey();
+            }
         }
     }
+
+
+    public class PlaceOrder : ReceivePersistentActor
+    {
+        public PlaceOrder() {}
+        public PlaceOrder(int orderId) { OrderId = orderId;
+
+            Receive(message => {
+
+                Console.WriteLine("here");
+
+            });
+        }
+
+
+        public List<String> Events { get; set; }
+        public int OrderId  { get; set; }
+     
+        public override string PersistenceId => $"{nameof(PlaceOrder)}-{OrderId}";
+
+
+        protected override  
+        
+    }
+    public class PlaceOrderItem
+    {
+
+    }
+    
 }
