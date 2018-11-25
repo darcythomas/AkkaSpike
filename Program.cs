121d466f5998d7e1f05bb507748d051ae125094d
Darcy <github@darcythomas.com>
2018-11-25 17:22:29 +1300

    wip full lifecycle

--- Program.cs
+++ Program.cs
@@ -57,7 +57,7 @@ namespace AkkaSpike
             {
                 string[] command = msg.ToLower().Split(' ');
 
-                var name = $"{nameof(OrderActor)}-{command.Skip(1).FirstOrDefault() ?? "0"}";
+                var orderId = command.Skip(1).FirstOrDefault() ?? "0";
 
                 // var orderActor = Program.system.ActorOf(Props.Create(() => new OrderActor(name)));
 
@@ -68,7 +68,7 @@ namespace AkkaSpike
                         break;
                     case "list":
 
-                        var orderActor = Program.system.ActorOf(Props.Create(() => new OrderActor(name)));
+                        var orderActor = Program.system.ActorOf(Props.Create(() => new OrderActor(orderId)));
                         orderActor.Tell(new GetStatus());
                         break;
                     case "place":
@@ -76,13 +76,14 @@ namespace AkkaSpike
                         var orderIdActor = Program.system.ActorOf(Props.Create(() => new OrderIdActor()));
                         orderIdActor.Tell(new OrderPlaced());
                         break;
-                    case "ship":
 
-                        var orderActor2 = Program.system.ActorOf(Props.Create(() => new OrderActor(name)));
-                        orderActor2.Tell(new OrderShipped());
+                    case "shipped":
+                        var orderActorS = Program.system.ActorOf(Props.Create(() => new OrderActor(orderId)));
+                        orderActorS.Tell(new OrderShipped());
                         break;
+
                     default:
-                        Console.WriteLine("try again");
+                        Console.WriteLine("try again", Color.Red);
                         break;
                 }
             }
@@ -91,7 +92,6 @@ namespace AkkaSpike
 
     public class OrderIdActor : ReceivePersistentActor
     {
-
         public int Id { get; set; }
 
         public OrderIdActor()
@@ -104,7 +104,6 @@ namespace AkkaSpike
                     var order = Program.system.ActorOf(Props.Create(() => new OrderActor($"{Id}")));
                     order.Tell(new OrderPlaced());
                 });
-
             });
 
             Recover<OrderPlaced>(message => Id++);
@@ -112,33 +111,96 @@ namespace AkkaSpike
 
         public override string PersistenceId => nameof(OrderIdActor);
     }
+    public class EmailActor : ReceivePersistentActor
+    {
+        private readonly string _orderId;
+
+        public EmailActor(string orderId)
+        {
+            _orderId = orderId;
+
+            Command<SendEmailConfirmation>(message => { Persist(message, m => { SendEmailConfirmation(m); }); });
+            Recover<SendEmailConfirmation>(message => SendEmailConfirmation(message));
+
+            Command<SendEmailItemShipped>(message => { Persist(message, m => { SendEmailItemShipped(m); }); });
+            Recover<SendEmailItemShipped>(message => SendEmailItemShippedRestored(message));
+        }
+
+        private void SendEmailItemShipped(SendEmailItemShipped message)
+        {
+            Console.WriteLine("Item Shipped", Color.Aqua);
+        }
+        private void SendEmailItemShippedRestored(SendEmailItemShipped message)
+        {
+            Console.WriteLine("Item Shipped (Restore)", Color.Beige);
+        }
+
+        private void SendEmailConfirmation(SendEmailConfirmation m)
+        {
+
+            if (!IsRecovering)
+            {
+                Console.WriteLine("Item Shipped", Color.Aqua);
+            }
+            else
+            {
+                Console.WriteLine("Item Shipped (Restore)", Color.Beige);
+            }
+        }
+
+        public override string PersistenceId => $"{nameof(EmailActor)}-{_orderId}";
+    }
+    public class PaymentActor : ReceivePersistentActor
+    {
+        private string _orderId;
+        public PaymentActor(string orderId)
+        {
+            _orderId = orderId;
+
+            Command<ProcessPayment>(message =>
+            {
+                Persist(message, m =>
+                {
+                    var order = Program.system.ActorOf(Props.Create(() => new OrderActor($"{_orderId}")));
+                    order.Tell(new PaymentCompleted());
+
+                    Console.WriteLine("Payment processed");
+                });
+
+            });
+        }
+
+        public override string PersistenceId => $"{nameof(PaymentActor)}-{_orderId}";
+    }
     public class OrderActor : ReceivePersistentActor
     {
 
         private List<string> EventLog = new List<string>();
+
         public OrderActor(string orderId)
         {
             OrderId = orderId;
 
-            Command<OrderPlaced>(message =>
-            {
-                Persist(message, m => HandleOrderPlaced(m));
-
-            });
-
+            Command<OrderPlaced>(message => { Persist(message, m => HandleOrderPlaced(m)); });
             Recover<OrderPlaced>(message => HandleOrderPlaced(message));
 
-            Command<OrderShipped>(message =>
-            {
-                Persist(message, m =>
-                {
-                    OrderShipped(m);
-                });
-
-            });
-
-
+            Command<OrderShipped>(message => { Persist(message, m => { OrderShipped(m); }); });
             Recover<OrderShipped>(message => OrderShipped(message));
+
+            Command<NotifyWarehouse>(message => { Persist(message, m => { NotifyWarehouse(m); }); });
+            Recover<NotifyWarehouse>(message => NotifyWarehouse(message));
+
+            Command<ProcessPayment>(message => { Persist(message, m => { ProcessPayment(m); }); });
+            Recover<ProcessPayment>(message => ProcessPayment(message));
+
+            Command<SendEmailConfirmation>(message => { Persist(message, m => { SendEmailConfirmation(m); }); });
+            Recover<SendEmailConfirmation>(message => SendEmailConfirmation(message));
+
+            Command<SendEmailItemShipped>(message => { Persist(message, m => { SendEmailItemShipped(m); }); });
+            Recover<SendEmailItemShipped>(message => SendEmailItemShipped(message));
+
+            Command<PaymentCompleted>(message => { Persist(message, m => { PaymentCompleted(m); }); });
+            Recover<PaymentCompleted>(message => PaymentCompleted(message));
 
 
             Command<GetStatus>(message =>
@@ -146,31 +208,54 @@ namespace AkkaSpike
                 Console.WriteAscii("Listing events", Color.Blue);
                 foreach (var item in EventLog)
                 {
-                    Console.WriteLine(item);
+                    Console.WriteLine(item, Color.BlueViolet);
                 }
 
             });
+        }
 
+        private void PaymentCompleted(PaymentCompleted message)
+        {
+            var order = Program.system.ActorOf(Props.Create(() => new EmailActor($"{OrderId}")));
+            order.Tell(new SendEmailConfirmation());
+        }
 
+        private void SendEmailItemShipped(SendEmailItemShipped message)
+        {
+            EventLog.Add("Order shipped, Email Sent");
+        }
 
+        private void SendEmailConfirmation(SendEmailConfirmation message)
+        {
+            EventLog.Add("Payment processed, Email Sent");
+        }
 
+        private void ProcessPayment(ProcessPayment m)
+        {
+            EventLog.Add("Payment processed");
+        }
+
+        private void NotifyWarehouse(NotifyWarehouse message)
+        {
+            EventLog.Add("Notify Warehouse");
         }
 
         private void OrderShipped(OrderShipped message)
         {
-            Console.WriteLine("OrderShipped");
-            EventLog.Add("OrderShipped");
+            EventLog.Add("Order Shipped");
+            var order = Program.system.ActorOf(Props.Create(() => new EmailActor($"{OrderId}")));
+            order.Tell(new SendEmailItemShipped());
         }
 
         private void HandleOrderPlaced(OrderPlaced message)
         {
-            if (!this.IsRecovering)
+            if (!IsRecovering)
             {
-                Console.WriteLine($"New OrderPlaced {OrderId}", Color.BlueViolet);
-
+                var order = Program.system.ActorOf(Props.Create(() => new PaymentActor($"{OrderId}")));
+                order.Tell(new ProcessPayment());
             }
-            Console.WriteLine("OrderPlaced");
-            EventLog.Add("OrderPlaced");
+            Console.WriteLine($"OrderPlaced {OrderId}");
+            EventLog.Add("Order Placed");
         }
 
 
@@ -179,10 +264,9 @@ namespace AkkaSpike
 
         public override string PersistenceId => $"{nameof(OrderActor)}-{OrderId}";
 
-
-
-
     }
+
+
     public class GetStatus
     {
 
@@ -196,6 +280,10 @@ namespace AkkaSpike
 
     }
     public class NotifyWarehouse
+    {
+
+    }
+    public class PaymentCompleted
     {
 
     }
